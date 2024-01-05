1ee900a72fdc90de78d9b18be8531f84f014ceb8
Sanjay <Sanjay.sharma@innogent.in>
2024-01-05 16:24:06 +0530

    Headless automation completed

--- cypress/e2e/1-getting-started/Randomtest.cy.js
+++ cypress/e2e/1-getting-started/Randomtest.cy.js
@@ -1,5 +1,5 @@
 //<reference types="cypress" />
-import Headless from "../Pageobject/Headlesscrop.cy";
+import Headless from "../Pageobject/Headlesscrop.cy.js";
 import Homepage from "../Pageobject/Homepage.cy";
 import { random_name } from "../Pageobject/Randomname.cy";
 
@@ -21,7 +21,7 @@ describe('Login', function hdless() {
 
     })
 
-    it('userlogin', function() {
+    it('userlogin', function () {
 
         cy.viewport(1200, 800);
         //visit URL
@@ -39,10 +39,91 @@ describe('Login', function hdless() {
         //clicks on Automation
         headless.headlessAutomation().click()
 
-        for(let i =0; i <50 ; i++){
+
         //Select the image and upload
         cy.get('input[type=file]').selectFile('cypress/fixtures/UploadImages/image2.jpeg', { force: true })
-        }
+
+        cy.wait(7000)
+
+        cy.contains("Next: Choose Crop Marker").click()
+
+        cy.get(".ant-select-selector").type("Face")
+
+        cy.get("div.ant-select-item.ant-select-item-option.ant-select-item-option-active > div > span").each(($e1, index, $list) => {
+            if ($e1.text() === "Face") {
+                $e1.click()
+            }
+        })
+
+
+        //crop area top
+        cy.get(':nth-child(1) > .ant-input-number-affix-wrapper').clear().type("50%")
+
+        //crop area left
+        cy.get('.crop-area_inputWrapperMiddle__jgWW7 > :nth-child(1)').clear().type("50%")
+
+        //crop area down
+        cy.get(':nth-child(3) > .ant-input-number-affix-wrapper').clear().type("50%")
+
+        //crop area right
+        cy.get('.crop-area_inputWrapperMiddle__jgWW7 > :nth-child(3)').clear().type("50%")
+
+        cy.contains("Next: Choose Background").click()
+
+        //keep original background
+        cy.get('#backgrond-settings-radio0 > .ant-radio-wrapper > .ant-radio > .ant-radio-input').click()
+
+        //keep Transparent
+        cy.get('#backgrond-settings-radio1 > .ant-radio-wrapper > .ant-radio > .ant-radio-input').click()
+
+        //Keep White
+        cy.get('#backgrond-settings-radio2 > .ant-radio-wrapper > .ant-radio > .ant-radio-input').click()
+
+        //Keep solid 
+        cy.get('#backgrond-settings-radio3 > .ant-radio-wrapper > .ant-radio > .ant-radio-input').click()
+
+        cy.contains("Next: Choose Crop Size").click()
+
+        //Square output size (1080*1080)
+        cy.get('#customFormats > :nth-child(1) > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input').click()
+
+        //Landscape output size(1080*540)
+        cy.get(':nth-child(2) > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input').click()
+
+        //portait output size(540*1080)
+        cy.get(':nth-child(3) > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input').click()
+
+        //amazon output size(2000*2000)
+        cy.get(':nth-child(4) > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input').click()
+
+
+        //Advance setting
+        cy.get('.ant-collapse-expand-icon').click()
+
+        //Click on output format
+        cy.get('.select-format_advancedSettingsContainer__Yscvy > :nth-child(1) > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input').click()
+
+        //Auto
+        cy.get(':nth-child(1) > .ant-radio-wrapper > .ant-radio > .ant-radio-input').click()
+
+        //PNG
+        cy.get(':nth-child(2) > .ant-radio-wrapper > .ant-radio > .ant-radio-input').click()
+
+        //JPEG
+        cy.get(':nth-child(3) > .ant-radio-wrapper > .ant-radio > .ant-radio-input').click()
+
+        //WEBP
+        cy.get(':nth-child(1) > .ant-radio-wrapper > .ant-radio > .ant-radio-input').click()
+
+        //Check Quality
+        cy.get('.select-format_advancedSettingsContainer__Yscvy > :nth-child(3) > .ant-checkbox-wrapper > .ant-checkbox > .ant-checkbox-input').click()
+
+        //quality type
+        cy.get('.ant-input-number-input').clear().type("5")
+
+        cy.contains("Next: Review Settings").click()
+
+        cy.contains("Start Crop").click()
 
 
 
